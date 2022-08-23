876b3ba03109af1fb8f0e70e4b9cd7dcdaaf1fb8
Kenneth Gerardo García Lemus <kgarcia-2020050@kinal.edu.gt>
2022-08-23 09:05:10 -0600

    Primer commit

---
+++ Java/src/main/java/com/is4tech/practicas/ProyectoParaPracticasApplication.java
@@ -0,0 +1,13 @@
+package com.is4tech.practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoParaPracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoParaPracticasApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/com/is4tech/practicas/ProyectoParaPracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is4tech.practicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoParaPracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
